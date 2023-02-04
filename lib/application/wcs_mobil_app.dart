c9198e9f2631d1d6d4eadd27dedec593ed18e95f
westito <nemeth@marcell.it>
2023-02-04 14:01:46 +0100

    Convert to bloc

--- lib/application/wcs_mobil_app.dart
+++ lib/application/wcs_mobil_app.dart
@@ -1,39 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:wcs_mobil/application/app_theme.dart';
-import 'package:wcs_mobil/domain/entities/driver.dart';
-import 'package:wcs_mobil/ui/driver/driver_screen.dart';
-import 'package:wcs_mobil/ui/home/home_screen.dart';
-import 'package:wcs_mobil/ui/season/season_screen.dart';
 
-class WCSMobilApp extends ConsumerWidget {
+class WCSMobilApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return MaterialApp.router(
-      routerConfig: ref.read(_routerProvider),
+      routerConfig: context.watch<GoRouter>(),
       theme: appThemeDark,
     );
   }
 }
-
-final _routerProvider = Provider((ref) {
-  return GoRouter(
-    routes: [
-      GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
-      GoRoute(
-        path: '/season/:year',
-        builder: (context, state) => SeasonScreen(year: state.params['year']!),
-      ),
-      GoRoute(
-        path: '/season/:year/driver/:name',
-        builder: (context, state) =>
-            DriverScreen(driver: state.extra! as Driver),
-      ),
-    ],
-  );
-});
 
 Future<void> initSystemOverlay() async {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
